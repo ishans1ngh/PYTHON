cf3f7107905815d9becb3895da0760614ea91d88
ishans1ngh <ishansingh.one8@gmail.com>
2025-09-01 10:30:15 +0530

    day 3

---
+++ sequencing.py
@@ -0,0 +1,9 @@
+list =  ["abcd", 1234, 56.78]
+
+print(list)
+print(list[0])
+print(list[1:3])
+print(list[::-1])
+
+for i in range(len(list)):
+    print(list[i])
---
+++ string.py
@@ -0,0 +1,12 @@
+name = "Ishan Singh"
+
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.replace("Ishan", "Aman"))
+print(name.split())
+print(f"my name is {name}")
+
+a = str(11/2)
+print(a)
+print(type(a))
